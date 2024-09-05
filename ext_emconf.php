24db83f47cab8ed2adb329f1269b78a600e47230
Juliane Wundermann <j.wundermann@familie-redlich.de>
2024-09-05 12:50:54 +0200

    [RELEASE] Release of cps_myra_cloud 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Familie-Redlich Digital GmbH',
     'state' => 'alpha',
     'clearCacheOnLoad' => 0,
-    'version' => '0.7.1',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
